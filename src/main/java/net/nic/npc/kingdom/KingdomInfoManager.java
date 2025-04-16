d4fe9dca3297e02bf59e39cd80f45427b5cf97bd
Niccokay-0 <eanicgalt@gmail.com>
2025-04-16 22:57:28 +0200

    Comments Del

--- src/main/java/net/nic/npc/kingdom/KingdomInfoManager.java
+++ src/main/java/net/nic/npc/kingdom/KingdomInfoManager.java
@@ -9,7 +9,6 @@ import java.util.UUID;
 
 public class KingdomInfoManager {
 
-    // Nested map: World -> Player -> KingdomInfo instance
     private static final Map<ServerLevel, Map<UUID, KingdomInfo>> INFO_MAP = new HashMap<>();
 
     public static KingdomInfo get(ServerPlayer player) {
@@ -19,7 +18,6 @@ public class KingdomInfoManager {
         INFO_MAP.putIfAbsent(world, new HashMap<>());
         Map<UUID, KingdomInfo> playerMap = INFO_MAP.get(world);
 
-        // If the KingdomInfo instance doesn't exist yet, create and store one
         if (!playerMap.containsKey(playerUUID)) {
             playerMap.put(playerUUID, new KingdomInfo());
         }
